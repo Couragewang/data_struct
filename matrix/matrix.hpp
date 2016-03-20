d40783ed553aceb34b2322042dfefbb937818472
hb <295851252@qq.com>
2016-03-20 13:50:21 +0800

    add matrix

--- matrix/matrix.hpp
+++ matrix/matrix.hpp
@@ -3,4 +3,30 @@
 #include <iostream>
 using namespace std;
 
-
+//对称矩阵存储，存储下三角
+template<class T>
+class symmetric_matrix{
+	public:
+		symmetric_matrix(unsigned int n = N)//n*n矩阵
+			:n(_n), size(_n*(_n+1)/2)    //计算压缩存贮的节点个数
+		{
+			matrix = new T[size];
+		}
+		bool restore(T arr[][N], int line = N)
+		{
+			for(int i = 0; i <= line, i++){
+				for(int j = 0;){
+				}
+			}
+		}
+		~symmetric_matrix()
+		{
+			if(matrix){
+				delete []matrix;
+			}
+		}
+	private:
+		size_t n;   //矩阵阶数
+		size_t size;//存储数组的大小
+		T* matrix;
+};
