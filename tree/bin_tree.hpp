a1fa273c55ba82919cece9c66d2fa33fe5761726
hb <295851252@qq.com>
2016-04-01 15:29:51 +0800

    完成基本的tree操作

--- tree/bin_tree.hpp
+++ tree/bin_tree.hpp
@@ -35,12 +35,13 @@ class bin_tree{
 		//先序建立二叉树
 		bin_tree_node<T> *_create_bin_tree(const T *_arr, const size_t &_size, size_t &_index)
 		{
+			bin_tree_node<T> *tmp = NULL;
 			if(_index < _size && _arr[_index] != '#'){
-				bin_tree_node<T> *tmp  = _buy_node(_arr[_index]);
+				tmp  = _buy_node(_arr[_index]);
 				tmp->left_child = _create_bin_tree(_arr, _size, ++_index);
 				tmp->right_child = _create_bin_tree(_arr, _size, ++_index);
 			}
-			return NULL;
+			return tmp;
 		}
 
 		//前序遍历二叉树
@@ -75,6 +76,62 @@ class bin_tree{
 		    _post_order(_root->right_child);
 			visit(_root->data);
 		}
+		//清除二叉树
+		void _destroy(bin_tree_node<T> *&_root)
+		{
+			if( NULL == _root ){
+				return;
+			}
+			_destroy(_root->left_child);
+			_destroy(_root->right_child);
+			delete _root;
+		} 
+
+		bin_tree_node<T> *_find(bin_tree_node<T> *_root, const T &_val)
+		{
+			if( NULL == _root ){
+				return NULL;
+			}
+			if( _root->data == _val ){
+				return _root;
+			}
+			bin_tree_node<T> *tmp = _find(_root->left_child, _val);//递归在左子树查找
+			if( !tmp ){
+				return _find(_root->right_child, _val); //递归在右子树查找
+			}
+			return NULL;
+		}
+
+		int _heigth(bin_tree_node<T> *_root)
+		{
+			if( NULL == _root ){
+				return 0;
+			}
+			int curr_left_heigth = _heigth(_root->left_child) + 1;
+			int curr_right_heigth = _heigth(_root->right_child) + 1;
+
+			return curr_left_heigth > curr_right_heigth?curr_left_heigth:curr_right_heigth;
+		}
+
+		int _size(bin_tree_node<T> *_root)
+		{
+			if( NULL == _root ){
+				return 0;
+			}
+			return _size(_root->left_child) + _size(_root->right_child) + 1;
+		}
+
+		//拷贝二叉树
+		bin_tree_node<T>* _copy(bin_tree_node<T> *_root)
+		{
+			bin_tree_node<T> *new_root = NULL;
+			if(_root){
+				new_root = _buy_node(_root->data);
+				new_root->left_child = _copy(_root->left_child);
+				new_root->right_child = _copy(_root->right_child);
+			}
+			return new_root;
+		}
 
 	public:
 		//构造函数1
@@ -89,6 +146,24 @@ class bin_tree{
 			root = _create_bin_tree(arr, size, index);
 		}
 
+		//拷贝构造
+		bin_tree(const bin_tree<T> &t)
+		{
+			this->root = _copy(t.root);
+		}
+
+		//赋值语句
+		const bin_tree<T> &operator=(bin_tree<T> t)
+		{
+			swap(this->root, t.root);
+			return (*this);
+		}
+
+		~bin_tree()
+		{
+			//清除数结构
+			_destroy(root);
+		}
 		void prev_order()
 		{
 			cout<<"prev order: ";
@@ -109,6 +184,22 @@ class bin_tree{
 			_post_order(root);
 			cout<<endl;
 		}
+
+		int size()
+		{
+			return _size(this->root);
+		}
+
+		int heigth()
+		{
+			return _heigth(this->root);
+		}
+
+		bin_tree_node<T>* find(const T &val)
+		{
+			return _find(this->root, val);
+		}
+
 	public:
 		bin_tree_node<T> *root; //树根 
 };
--- tree/main.cpp
+++ tree/main.cpp
@@ -3,8 +3,30 @@
 int main()
 {
 	char tree[]="123##4##56";
+	cout<<"++++++++++++++基本++++++++++++++"<<endl;
 	bin_tree<char> mytree(tree, strlen(tree));
-
 	mytree.prev_order();
+	mytree.in_order();
+	mytree.post_order();
+	cout<<"size: "<<mytree.size()<<endl;
+	cout<<"heigth: "<<mytree.heigth()<<endl;
+
+	cout<<"++++++++++++++拷贝++++++++++++++"<<endl;
+	bin_tree<char> copy_tree(mytree);
+	copy_tree.prev_order();
+	copy_tree.in_order();
+	copy_tree.post_order();
+	cout<<"size: "<<copy_tree.size()<<endl;
+	cout<<"heigth: "<<copy_tree.heigth()<<endl;
+
+	cout<<"++++++++++++++赋值++++++++++++++"<<endl;
+	bin_tree<char> _copy_tree;
+	_copy_tree = mytree;
+	_copy_tree.prev_order();
+	_copy_tree.in_order();
+	_copy_tree.post_order();
+	cout<<"size: "<<_copy_tree.size()<<endl;
+	cout<<"heigth: "<<_copy_tree.heigth()<<endl;
+
 	return 0;
 }
