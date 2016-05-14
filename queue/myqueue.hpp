3ea991455685bad150f95babfb98d55af11fe457
hb <295851252@qq.com>
2016-05-14 16:41:41 +0800

    完成queue

--- queue/myqueue.hpp
+++ queue/myqueue.hpp
@@ -2,10 +2,119 @@
 
 //链表的链式实现
 #include <iostream>
-typedef int elem_type;
 
+template <class T>
 class myqueue{
-	private:
+	typedef struct _node{
+		T _data;
+		struct _node *_next;
+
+		_node(const T &_val)
+			:_data(_val)
+			,_next(NULL)
+		{}
+	}node_t, *node_p;
+
+	node_p alloc_node(const T &_val)
+	{
+		node_p tmp = new node_t(_val);
+		return tmp;
+	}
+
+	void delete_node(node_p &tmp)
+	{
+		if( tmp ){
+			delete tmp;
+			tmp = NULL;
+		}
+	}
 
 	public:
+		myqueue()
+		{
+			head = tail = NULL;
+			size = 0;
+		}
+
+		void push(const T &_val)
+		{
+			//队列中没有节点
+			if( size == 0 ){
+				head = alloc_node(_val);
+				tail = head;
+			}else{//有节点
+				tail->_next = alloc_node(_val);
+				tail = tail->_next;
+			}
+			size++;
+		}
+
+		void pop()
+		{
+			if( size == 0 ){
+				return;
+			}else if( size == 1 ){
+				delete_node(head);
+				head = tail = NULL;
+			}else{
+				node_p tmp = head;
+				head = head->_next;
+				delete_node(tmp);
+			}
+			--size;
+		}
+
+		const T &back()
+		{
+			assert(tail);
+			return tail->_data;
+		}
+
+		const T &front()
+		{
+			assert(head);
+			return head->_data;
+		}
+
+		bool empty()
+		{
+			return size == 0;
+		}
+
+		void clear()
+		{
+			while(size > 0){
+				pop();
+			}
+		}
+
+		size_t queue_size()
+		{
+			return size;
+		}
+
+		void print()
+		{
+			node_p begin = head;
+			std::cout<<"queue head <- ";
+			while(begin){
+				std::cout<<begin->_data<<" <- ";
+				begin = begin->_next;
+			}
+			std::cout<<"queue tail"<<std::endl;
+		}
+
+		~myqueue()
+		{
+			clear();
+		}
+	private:
+		node_p head; //队列头部
+		node_p tail; //队列尾部
+		size_t size; //队列中元素个数
 };
+
+
+
+
+
--- queue/test_main.cpp
+++ queue/test_main.cpp
@@ -5,6 +5,39 @@ using namespace std;
 
 int main()
 {
-	cout<<"hello world"<<endl;
+	myqueue<int> q;
+	q.push(1);
+	q.print();
+	q.push(2);
+	q.print();
+	q.push(3);
+	q.print();
+	q.push(4);
+	q.print();
+	q.push(5);
+	q.print();
+	q.push(6);
+	q.print();
+	q.push(7);
+	q.print();
+	q.push(8);
+	q.print();
+
+	q.pop();
+	q.print();
+	q.pop();
+	q.print();
+	q.pop();
+	q.print();
+	q.pop();
+	q.print();
+	q.pop();
+	q.print();
+	q.pop();
+	q.print();
+	q.pop();
+	q.print();
+	q.pop();
+	q.print();
 	return 0;
 }
