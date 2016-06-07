df3b48f915e527a3a019ae0e33d4fc4a44a60c63
hb <295851252@qq.com>
2016-06-07 14:28:31 +0800

    新增红黑树

--- tree/rb_tree/rb_tree.hpp
+++ tree/rb_tree/rb_tree.hpp
@@ -2,8 +2,8 @@
 /// \@红黑树的五个性质：
 /// \@1）每个结点要么是红的，要么是黑的。
 /// \@2）根结点是黑的。
-/// \@3）每个叶结点，即空结点（NIL）是黑的。
-/// \@4）如果一个结点是红的，那么它的俩个儿子都是黑的。
+/// \@3）每个叶结点，即空结点（NIL）是黑的(注意！ 这里的叶节点不是我们度为0的节点，而是NULL指针)。
+/// \@4）如果一个结点是红的，那么它的俩个儿子都是黑的(从每个叶子到根的所有路径上不能有两个连续的红色节点)。
 /// \@5）对每个结点，从该结点到其子孙结点的所有路径上包含相同数目的黑结点。
 
 #include <iostream>
@@ -30,7 +30,7 @@ namespace RB{
 		_rb_node(const K & _key = K(), const V &_value = V() )
 			:key(_key)
 			,value(_value)
-			,color(RED)
+			,color(RED) //新节点默认颜色是红色，想想为什么?
 			,left(NULL)
 			,right(NULL)
 			,parent(NULL)
@@ -58,13 +58,11 @@ namespace RB{
 					_node = NULL;
 				}
 			}
-
 			node_p alloc_node(const K &_key, const V &_value)
 			{
 				node_p tmp = new node_t(_key, _value);
 				return tmp;
 			}
-
 			void _destroy(node_p &_root)
 			{
 				if(_root){
@@ -73,6 +71,13 @@ namespace RB{
 					_delete_node(_root);
 				}
 			}
+
+			//插入新节点总是红色节点, 因为不会破坏性质 5, 尽可能维持所有性质.
+			bool insert(const K &_key, const V &_value)
+			{
+
+			}
+
 			node_p root;//红黑树树根
 	};
 
